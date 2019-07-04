f8042d64d272baee3ffd5ea0bea711d173e2d306
Christoph Frick <cf@ofnir.net>
2019-07-04 21:15:44 +0200

    Init

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject net.ofnir.default/lein-template "RELEASE"
+  :description "Lein template for net.ofnir Clojure projects"
+  :eval-in-leiningen true)
---
+++ src/leiningen/new/net/ofnir/default.clj
@@ -0,0 +1,23 @@
+(ns leiningen.new.net.ofnir.default
+  (:require [leiningen.new.templates :refer [renderer year date project-name ->files sanitize-ns name-to-path multi-segment]]
+            [leiningen.core.main :as main]))
+
+(defn net.ofnir.default
+  [name]
+  (let [render (renderer "net/ofnir/default")
+        main-ns (multi-segment (sanitize-ns name))
+        data {:raw-name name
+              :name (project-name name)
+              :namespace main-ns
+              :nested-dirs (name-to-path main-ns)
+              :year (year)
+              :date (date)}]
+    (main/info "Generating a project called" name "based on a modified 'default' template.")
+    (->files data
+             ["project.clj" (render "project.clj" data)]
+             ["README.md" (render "README.md" data)]
+             [".gitignore" (render "gitignore" data)]
+             ["src/{{nested-dirs}}.clj" (render "core.clj" data)]
+             ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
+             ["LICENSE" (render "LICENSE" data)]
+             "resources")))
