d77b4537cfc0ab1d090766c4149282e2c873d7bb
Christoph Frick <cf@ofnir.net>
2021-03-24 23:11:05 +0100

    Update deps in template

--- resources/leiningen/new/net/ofnir/default/project.clj
+++ resources/leiningen/new/net/ofnir/default/project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/christoph-frick/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/test.check "1.1.0"]]
-  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.0.732"]]}}
+  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.0.829"]]}}
   :aliases {"test-refresh" ["kaocha" "--watch"]
             "kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
   :global-vars {*warn-on-reflection* true
