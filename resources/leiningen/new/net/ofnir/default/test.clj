92cebdc26425d2c9c474ddaec4899d3c05b1742d
Christoph Frick <cf@ofnir.net>
2021-04-06 10:00:22 +0200

    Don't add a failing test; adjust test requires to current snippet

--- resources/leiningen/new/net/ofnir/default/test.clj
+++ resources/leiningen/new/net/ofnir/default/test.clj
@@ -1,7 +1,3 @@
 (ns {{namespace}}-test
-  (:require [clojure.test :refer :all]
-            [{{namespace}} :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (:require [clojure.test :refer [deftest testing is are]]
+            [{{namespace}} :as sut]))
