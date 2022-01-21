52a84ca6975992f30fdae2369572af6bbf85fdb9
Christoph Frick <cf@ofnir.net>
2022-01-21 18:05:50 +0100

    Update deps

--- resources/leiningen/new/net/ofnir/default/project.clj
+++ resources/leiningen/new/net/ofnir/default/project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/test.check "1.1.0"]]
-  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.0.829"]]}}
+                 [org.clojure/test.check "1.1.1"]]
+  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.60.977"]]}}
   :aliases {"test-refresh" ["kaocha" "--watch"]
             "kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
   :global-vars {*warn-on-reflection* true
