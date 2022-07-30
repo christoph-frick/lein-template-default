141f67e031982cf6d3e91a18f8c0b26614c20f25
Christoph Frick <cf@ofnir.net>
2022-07-30 12:52:32 +0200

    Rename GA to work with known group on clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.ofnir.default/lein-template "RELEASE"
+(defproject net.ofnir/lein-template.default "RELEASE"
   :description "Lein template for net.ofnir Clojure projects"
   :eval-in-leiningen true
   :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}})
