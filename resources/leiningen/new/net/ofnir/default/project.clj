f9d4f954ed0ef69bf531e1da298a691008de4349
Christoph Frick <cf@ofnir.net>
2022-07-30 12:44:15 +0200

    Update versions

--- resources/leiningen/new/net/ofnir/default/project.clj
+++ resources/leiningen/new/net/ofnir/default/project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/christoph-frick/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/test.check "1.1.1"]]
-  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.60.977"]]}}
   :aliases {"test-refresh" ["kaocha" "--watch"]
+  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.69.1069"]]}}
             "kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* true})
