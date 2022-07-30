0317f5ad375e4a2d281a1ad846390721ac046fa8
Christoph Frick <cf@ofnir.net>
2022-07-30 12:49:31 +0200

    Add release repo

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject net.ofnir.default/lein-template "RELEASE"
   :description "Lein template for net.ofnir Clojure projects"
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}})
